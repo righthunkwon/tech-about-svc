fe2186518517895378ca6357d772d7ccba4360b5
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2026-03-03 23:44:40 +0900

    feat: project init

---
+++ src/main/java/com/techabout/TechaboutApplication.java
@@ -0,0 +1,13 @@
+package com.techabout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechaboutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechaboutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techabout/TechaboutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techabout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechaboutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
